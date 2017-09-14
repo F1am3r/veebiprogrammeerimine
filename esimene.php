56b8686f5c50b778ec6709c6cc7cce11707460ef
Jan-Ross Liver <F1am3r@users.noreply.github.com>
2017-09-14 10:47:07 +0300

    Esimene php kood lisatud

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jan-Rossi veebiprogrammerimise raames loodud leht</title>
+</head>
+<body>
+	<h1>Teretulemast minu lehele</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Kodus lisasin oma arvutist selle lõigu siia juurde</p>
+	<?php 
+		echo "Algas php õppimine!";
+	?>
+	
+
+</body>
+</html>
