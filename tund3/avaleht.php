318eb0f112b0c33291516b823c370b54b01a9128
Jan-Ross Liver <F1am3r@users.noreply.github.com>
2017-09-21 11:37:40 +0300

    tund3 kaust

---
+++ tund3/avaleht.php
@@ -0,0 +1,25 @@
+<?php
+	//kommentaare kasutame koodiblokkide nimetamiseks
+	//muutujad:
+	$myName = "Jan-Ross";
+	$myFamilyName = "Liiver";
+	
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jan-Rossi veebiprogrammerimise raames loodud leht</title>
+</head>
+<body>
+	<h1>Teretulemast minu, <?php echo $myName ." " .$myFamilyName;?>, lehele</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Kodus lisasin oma arvutist selle lõigu siia juurde</p>
+	<p>Meie õpime <a href="http://www.tlu.ee"> Tallinna Ülikoolis </a>.</p>
+	<p>Minu esimene php leht on <a href="../esimene.php">Siin</a></p>
+	<p>Petsensi leht on <a href="../../../~rooppeet/veebiprogrammeerimine/esimene.php">Siin</a></p>
+	<p>Lahe pildikogu on <a href="foto.php">Siin</a></p>
+</body>
+</html>
---
+++ tund3/foto.php
@@ -0,0 +1,39 @@
+<?php
+	//kommentaare kasutame koodiblokkide nimetamiseks
+	//muutujad:
+	$myName = "Jan-Ross";
+	$myFamilyName = "Liiver";
+	$picDir = "../../pics/";
+	$picFiles = [];
+	$picFileTypes = ["jpg", "jpeg", "png", "gif"];
+	
+	
+	$allFiles = array_slice(scandir($picDir),2);
+	foreach ($allFiles as $file){
+		$fileType = pathinfo($file, PATHINFO_EXTENSION);
+		if (in_array($fileType, $picFileTypes) == true){
+			array_push($picFiles, $file);
+		}
+	}
+	
+	//var_dump($allFiles);
+	//$picFiles = array_slice($allFiles,2);
+	//var_dump($picFiles);
+	$picFileCount = count($picFiles);
+	$picNumber = mt_rand(0,$picFileCount -1);
+	$picFile = $picFiles[$picNumber];
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jan-Rossi veebiprogrammerimise raames loodud leht</title>
+</head>
+<body>
+	<h1>Teretulemast minu, <?php echo $myName ." " .$myFamilyName;?>, lehele</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<img src="<?php echo $picDir . $picFile;?>" alt="Tallinna Ülikool">
+</body>
+</html>
